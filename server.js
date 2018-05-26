0dd2564aa2b02421ed8a4e04fc0c7ce061f4f36d
Tony Mugendi <mugendi.tony26@gmail.com>
2018-05-26 19:19:02 +0300

    New Commit

--- server.js
+++ server.js
@@ -1,65 +1,58 @@
-const express = require ('express');
-const hbs = require ('hbs');
+const express = require('express');
+const hbs  = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 3000;
-var app = express();
 
+var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+// app.use((req, res) => {
+//     res.render('maintain.hbs');
+// })
+
 
 app.use((req, res, next) => {
-    var now = new Date().toString()
-    var log = `${now}: ${req.method} ${req.url}`
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} ${req.url}`
+
     console.log(log);
     fs.appendFile('server.log', log + '\n', (err) => {
         if (err) {
-            console.log('Unable to append to server.log');
-            
+            console.log('Unable to Append File');
         }
-
     });
     next();
+    
 });
 
-// app.use((req, res) => {
-//     res.render('caution.hbs');
-
-// });
-
-app.use(express.static(__dirname + "/public"));
 
 
 hbs.registerHelper('currentYear', () => {
     return new Date().getFullYear();
 });
 
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
+
 app.get('/', (req, res) => {
     res.render('home.hbs', {
-        message: 'Welcome to my website',
+        welcome: 'Welcome to my website',
         pageTitle: 'Home Page',
-    });
-
-});
+    })
+})
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
     });
+})
 
-});
-
-app.get('/portfolio', (req, res) => {
-    res.render('portfolio.hbs', {
-        message: 'Check out my Projects',
-        pageTitle: 'Projects page'
-
-    });
-
-});
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
 });
